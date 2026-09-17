b51f61b0326f4cc98ebf468aa89c1545f60ad5c0
Sarah Kennedy <skendy05@byu.edu>
2026-09-17 00:19:46 -0600

    started rook functionality, made move maker to make new chess moves for the MoveType to append and returne

--- shared/src/main/java/chess/MoveType.java
+++ shared/src/main/java/chess/MoveType.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoveType {
     /*
         performs the calculations of each piece moves, and appends them as the end position in a ChessMove
@@ -9,10 +12,11 @@ public class MoveType {
         return List.of();
 
      */
-    public MoveType(){
-
-
+    public MoveType(ChessBoard board, ChessPiece piece, ChessPosition position) {
+        rookMove(board, piece, position);
     }
+
+
     /*
     1, get the piece's position - row and col (set them as a var)
     2, for each calc:
@@ -21,34 +25,62 @@ public class MoveType {
     3, return moveList
      */
 
+        //ChessMove function!!
+        // have conditional for pawn promos
+        public ChessMove moveMaker(ChessPosition position, int newRow, int newCol){
+            //ChessPosition newPos = ChessPosition(newRow,newCol);
 
-    //row and col is -1 in the get piece method to accommodate 0 indexing of the board
-    // so final positions must also be 1-9
+            return null;
+        }
 
-    //n is how many spaces we are moving, n must be 1-8
+        //row and col is -1 in the get piece method to accommodate 0 indexing of the board
+        // so final positions must also be 1-9
 
-    //bishops - bishopMove (diagonal moves)
-    //row+-n && col +-n, if neither would take it off the board
-    //(r || c) + n <= 8 && (r || c) - n > 0 [not indexed by 0]
-    //test looks like it checks (r+n, c+n) then (r-n, c-n)
+        //n is how many spaces we are moving, n must be 1-8
 
-    //rooks - rookMove (flat moves, horizontal/vertical)
-    //row +- n || col +- n if (r||c) + n >=9 or (r||c) - n >=1
+        //bishops - bishopMove (diagonal moves)
+        //row+-n && col +-n, if neither would take it off the board
+        //(r || c) + n <= 8 && (r || c) - n > 0 [not indexed by 0]
+        //test looks like it checks (r+n, c+n) then (r-n, c-n)
 
-    //queen - queenMove
-    // bishop + rook moves
+        //rooks - rookMove (flat moves, horizontal/vertical)
+        //row +- n || col +- n if (r||c) + n >=9 or (r||c) - n >=1
+        public List<ChessMove> rookMove(ChessBoard board, ChessPiece piece, ChessPosition position){
+            List<ChessMove> moveList = new ArrayList<>();
+             int col = position.getColumn();
+             int row = position.getRow();
+             int n = 1;
+             while (n < 8){
+                 //adding row
+                 int r = row + n;
+                 if ((1 <= r) && (r <= 9)){
+                     //make chess move
+                     ChessMove move = moveMaker(position, r, col);
 
-    //king - kingMove
-    //queen moves where n is hard coded as 1
+                     moveList.add(move);
+                 }
 
-    //knights - knightMove
-    // [row + 2, col +- 1], [row - 2, col +-1]
-    // [col + 2, row +- 1], [col -2, row +-1]
+                n++;
+            }
 
-    //pawns - pawnMove
-    // row + 1 (assuming the teams view their side as row 1)
-    // if other team piece at [row+1, col+-1], add to list
-    // if row + 1 = 9, get promotion
+            return moveList;
+
+        }
+
+        //queen - queenMove
+        // bishop + rook moves
+
+        //king - kingMove
+        //queen moves where n is hard coded as 1
+
+        //knights - knightMove
+        // [row + 2, col +- 1], [row - 2, col +-1]
+        // [col + 2, row +- 1], [col -2, row +-1]
+
+        //pawns - pawnMove
+        // row + 1 (assuming the teams view their side as row 1)
+        // if other team piece at [row+1, col+-1], add to list
+        // if row + 1 = 9, get promotion
 
 
 
